bfa9b716d2710cf571bb24877ef2890bdad1bca2
mikedelong <mikedelong@outlook.com>
2022-10-28 10:17:41 -0400

    feature: a first cut at the USA cause of death area plot. This is currently using a deprecated feature of matplotlib and will need to be redone; also the legend is a mess

--- cause_of_death.py
+++ cause_of_death.py
@@ -8,12 +8,47 @@ from logging import getLogger
 from arrow import now
 from pandas import DataFrame
 from pandas import read_csv
+from matplotlib.pyplot import style
+from matplotlib.pyplot import savefig
 
 
 def read_csv_dataframe(filepath_or_buffer: str) -> DataFrame:
     result_df = read_csv(filepath_or_buffer=filepath_or_buffer)
     return result_df
 
+
+COLUMNS = ['Entity', 'Code', 'Year', 'Number of executions (Amnesty International)',
+           'Deaths - Meningitis - Sex: Both - Age: All Ages (Number)',
+           "Deaths - Alzheimer's disease and other dementias - Sex: Both - Age: All Ages (Number)",
+           "Deaths - Parkinson's disease - Sex: Both - Age: All Ages (Number)",
+           'Deaths - Nutritional deficiencies - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Malaria - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Drowning - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Interpersonal violence - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Maternal disorders - Sex: Both - Age: All Ages (Number)',
+           'Deaths - HIV/AIDS - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Drug use disorders - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Tuberculosis - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Cardiovascular diseases - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Lower respiratory infections - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Neonatal disorders - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Alcohol use disorders - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Self-harm - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Exposure to forces of nature - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Diarrheal diseases - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Environmental heat and cold exposure - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Neoplasms - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Conflict and terrorism - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Diabetes mellitus - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Chronic kidney disease - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Poisonings - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Protein-energy malnutrition - Sex: Both - Age: All Ages (Number)', 'Terrorism (deaths)',
+           'Deaths - Road injuries - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Chronic respiratory diseases - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Cirrhosis and other chronic liver diseases - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Digestive diseases - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Fire, heat, and hot substances - Sex: Both - Age: All Ages (Number)',
+           'Deaths - Acute hepatitis - Sex: Both - Age: All Ages (Number)']
 
 DATA_FOLDER = './data/'
 INPUT_FILE = 'annual-number-of-deaths-by-cause.csv'
@@ -27,5 +62,17 @@ if __name__ == '__main__':
     input_file = DATA_FOLDER + INPUT_FILE
     df = read_csv_dataframe(filepath_or_buffer=input_file)
     LOGGER.info('loaded %d rows from %s', len(df), input_file)
+
+    # extract the data for just the USA, make the year the index
+    usa_df = df[df['Code'] == 'USA'].drop(columns=['Entity', 'Code'])
+    usa_df = usa_df.sort_values(by=['Year']).set_index(keys=['Year'])
+    # clean up the cause names
+    for column in list(usa_df):
+        new_column = column.replace(' - Sex: Both - Age: All Ages (Number)', '')
+        new_column = new_column.replace('Deaths - ', '')
+        usa_df.rename(inplace=True, columns={column: new_column})
+    style.use('seaborn')
+    usa_df.plot.area()
+    savefig(fname='./cause_of_death.png', format='png')
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
