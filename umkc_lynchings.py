250220f0e56297e5212283ded7220aa0d10a6464
mikedelong <mikedelong@outlook.com>
2022-10-29 15:43:20 -0400

    refactor: make the figsize a variable feature: add a citation to the graphs feature: move the figures to their own output folder

--- umkc_lynchings.py
+++ umkc_lynchings.py
@@ -9,6 +9,7 @@ from typing import Optional
 from arrow import now
 from matplotlib.pyplot import savefig
 from matplotlib.pyplot import subplots
+from matplotlib.pyplot import title
 from pandas import DataFrame
 from pandas import read_html
 
@@ -18,6 +19,8 @@ def get_html_dataframe(url: str, skiprows: Optional[int]) -> list[DataFrame]:
     return result_df
 
 
+FIGSIZE = (16, 8)
+OUTPUT_FOLDER = './plot_lynching/'
 URL = 'http://law2.umkc.edu/faculty/projects/ftrials/shipp/lynchingyear.html'
 
 if __name__ == '__main__':
@@ -26,26 +29,30 @@ if __name__ == '__main__':
     basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
     LOGGER.info('started')
 
+    # todo create output folder if it does not exist
+
     df = get_html_dataframe(url=URL, skiprows=3)
     df = df[0].dropna().iloc[0:87]
     for column in df.columns:
         df[column] = df[column].astype(int)
     df['Year'] = df['Year'].astype(int)
 
-    fig, ax = subplots(figsize=(16, 9))
+    fig, ax = subplots(figsize=FIGSIZE)
     ax.stackplot(df['Year'].values, df['Whites'].values, df['Blacks'].values, labels=['White', 'Black'])
     ax.legend(loc='upper right')
-    savefig(fname='./umkc_lynchings.png', format='png')
+    title('source: {}'.format(URL))
+    savefig(fname=OUTPUT_FOLDER + 'umkc_lynchings.png', format='png')
     del fig
 
     df['cumulative_white'] = df['Whites'].cumsum()
     df['cumulative_black'] = df['Blacks'].cumsum()
-    fig, ax = subplots(figsize=(16, 9))
+    fig, ax = subplots(figsize=FIGSIZE)
     ax.stackplot(df['Year'].values,
                  df['cumulative_black'].values,
                  df['cumulative_white'].values,
                  labels=['Black (cumulative)', 'White (cumulative)', ])
     ax.legend(loc='upper left')
-    savefig(fname='./umkc_lynchings_cumsum.png', format='png')
+    title('source: {}'.format(URL))
+    savefig(fname=OUTPUT_FOLDER + 'umkc_lynchings_cumsum.png', format='png')
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
