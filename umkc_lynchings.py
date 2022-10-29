bee6902e09a877d7801d28e4a0ded4579f89d24a
mikedelong <mikedelong@outlook.com>
2022-10-29 15:24:52 -0400

    feature: first working version to make a graph and save it

--- umkc_lynchings.py
+++ umkc_lynchings.py
@@ -1,5 +1,5 @@
 """
-Load and parse Excel data
+Load and parse HTML data
 """
 from logging import INFO
 from logging import basicConfig
@@ -7,6 +7,8 @@ from logging import getLogger
 from typing import Optional
 
 from arrow import now
+from matplotlib.pyplot import savefig
+from matplotlib.pyplot import subplots
 from pandas import DataFrame
 from pandas import read_html
 
@@ -25,6 +27,14 @@ if __name__ == '__main__':
     LOGGER.info('started')
 
     df = get_html_dataframe(url=URL, skiprows=3)
-    df = df[0].dropna().iloc[0:88]
+    df = df[0].dropna().iloc[0:87]
+    for column in df.columns:
+        df[column] = df[column].astype(int)
+    df['Year'] = df['Year'].astype(int)
+
+    fig, ax = subplots(figsize=(16, 9))
+    ax.stackplot(df['Year'].values, df['Whites'].values, df['Blacks'].values, labels=['White', 'Black'])
+    ax.legend(loc='upper left')
+    savefig(fname='./umkc_lynchings.png', format='png')
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
