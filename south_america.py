295e36f73c943702dbfd517827ffac84b968f81a
Mike DeLong <mikedelong@outlook.com>
2022-11-15 10:55:50 -0500

    cleanup: remove an embarrassing amount of dead code

--- south_america.py
+++ south_america.py
@@ -12,20 +12,17 @@ from arrow import now
 from matplotlib.pyplot import close
 from matplotlib.pyplot import savefig
 from matplotlib.pyplot import subplots
-from matplotlib.pyplot import tight_layout
-from numpy import nan
 from pandas import DataFrame
-from pandas import melt
 from pandas import read_excel
 from scipy.stats import linregress
 from seaborn import lineplot
 from seaborn import lmplot
 from seaborn import regplot
-from seaborn import relplot
 from seaborn import scatterplot
 from seaborn import set_style
 
 from common import COLUMNS
+
 
 def make_plots(column_name: str, column_short_name: str, input_df: DataFrame, fname_short: str,
                scale: Optional[int] = 1) -> float:
@@ -88,105 +85,27 @@ if __name__ == '__main__':
     LOGGER.info('loaded %d rows from %s', len(df), data_file)
 
     # for Latin America the location code is 904
-    latin_america_df =  df[(df['Parent code'] == 904) | (df['Location code'] == 904)][['Year', 'Region, subregion, country or area *',
-                                             'Natural Change, Births minus Deaths (thousands)',
-                                             'Rate of Natural Change (per 1,000 population)',
-                                             'Crude Death Rate (deaths per 1,000 population)',
-                                             ]].rename(columns={
+    latin_america_df = df[(df['Parent code'] == 904) | (df['Location code'] == 904)][
+        ['Year', 'Region, subregion, country or area *',
+         'Natural Change, Births minus Deaths (thousands)',
+         'Rate of Natural Change (per 1,000 population)',
+         'Crude Death Rate (deaths per 1,000 population)',
+         ]].rename(columns={
         'Crude Death Rate (deaths per 1,000 population)': 'Crude Death',
         'Region, subregion, country or area *': 'Region',
     })
-    latin_america_df['Region'] = latin_america_df['Region'].replace({'LATIN AMERICA AND THE CARIBBEAN': 'LATIN AMERICA'})
+    latin_america_df['Region'] = latin_america_df['Region'].replace(
+        {'LATIN AMERICA AND THE CARIBBEAN': 'LATIN AMERICA'})
 
     set_style(style=SEABORN_STYLE)
     figure_lineplot, axes_lineplot = subplots()
-    result_lineplot = lineplot(data=latin_america_df,  x='Year', y='Crude Death', hue='Region',)
+    result_lineplot = lineplot(data=latin_america_df, x='Year', y='Crude Death', hue='Region', )
 
     savefig(fname=OUTPUT_FOLDER + 'latin_america_lineplot.png', format='png')
     close(fig=figure_lineplot)
 
-
-    # figure_, axes_ = subplots()
-    # _ = lineplot(ax=axes_, data=regions_df, x='Year', y='Crude Death', hue='Region')
-    # fname_ = '{}{}_lineplot.png'.format(OUTPUT_FOLDER, 'region_crude_death')
-    # savefig(format='png', fname=fname_, )
-    # close(fig=figure_)
-    # LOGGER.info('saved plot in %s', fname_)
-
     # first get the Asian subregions
     t0 = df[df['Parent code'] == 904]['Location code'].unique()
     t1 = df[df['Parent code'].isin(t0)]['Location code'].unique()
-
-    # asia_subregion_df = df[df['Parent code'] == 935][[
-    #     'Year', 'Region, subregion, country or area *',
-    #     'Parent code',
-    #     'Natural Change, Births minus Deaths (thousands)',
-    #     'Rate of Natural Change (per 1,000 population)',
-    #     'Crude Death Rate (deaths per 1,000 population)',
-    # ]].rename(columns={
-    #     'Crude Death Rate (deaths per 1,000 population)': 'Crude Death',
-    #     'Region, subregion, country or area *': 'Region',
-    # })
-    # figure_, axes_ = subplots()
-    # _ = lineplot(ax=axes_, data=asia_subregion_df, x='Year', y='Crude Death', hue='Region')
-    # fname_ = '{}{}_lineplot.png'.format(OUTPUT_FOLDER, 'asia_subregion_crude_death')
-    # savefig(format='png', fname=fname_, )
-    # close(fig=figure_)
-    # LOGGER.info('saved plot in %s', fname_)
-
-    # Parent code 906
-    # eastern_asia_df = df[df['Parent code'] == 906][[
-    #     'Year', 'Region, subregion, country or area *',
-    #     'Crude Death Rate (deaths per 1,000 population)',
-    #     'Total Deaths (thousands)',
-    # ]].rename(columns={
-    #     'Crude Death Rate (deaths per 1,000 population)': 'Crude Death',
-    #     'Region, subregion, country or area *': 'Country',
-    #     'Total Deaths (thousands)': 'Deaths'
-    # })
-    # eastern_asia_df['Deaths'] = 1000 * eastern_asia_df['Deaths'].astype(int)
-    # eastern_asia_df['Year'] = eastern_asia_df['Year'].astype(int)
-    # # rename the countries so we get a small legend
-    # eastern_asia_df['Country'].replace({'China, Hong Kong SAR': 'Hong Kong',
-    #                                     'China, Macao SAR': 'Macau',
-    #                                     'China, Taiwan Province of China': 'Taiwan',
-    #                                     'Dem. People\'s Republic of Korea': 'N. Korea',
-    #                                     'Republic of Korea': 'S. Korea'}, inplace=True)
-    #
-    # figure_, axes_ = subplots()
-    # _ = lineplot(ax=axes_, data=eastern_asia_df, x='Year', y='Crude Death', hue='Country')
-    # fname_ = '{}{}_lineplot.png'.format(OUTPUT_FOLDER, 'eastern_asia_crude_death')
-    # tight_layout()
-    # savefig(format='png', fname=fname_, )
-    # close(fig=figure_)
-    # LOGGER.info('saved plot in %s', fname_)
-    #
-    # plot_df = melt(frame=eastern_asia_df, id_vars=['Year', 'Country'], value_name='Quantity',
-    #                value_vars=['Crude Death', 'Deaths'])
-    # figure_relplot, axes_relplot = subplots()
-    # result_relplot = relplot(col='variable', data=plot_df, kind='line', x='Year', y='Quantity', hue='Country',
-    #                          facet_kws={'sharey': False, 'sharex': True, 'legend_out': True, }, )
-    # result_relplot.set(ylabel=None)
-    # savefig(fname=OUTPUT_FOLDER + 'eastern_asia_relplot.png', format='png')
-    # close(fig=figure_relplot)
-    #
-    # china_df = plot_df[
-    #     (plot_df['Country'] == 'China') & (plot_df['variable'] != 'Crude Death') & (plot_df['Year'] >= 1956) & (
-    #             plot_df['Year'] <= 1964)].copy(deep=True)
-    # china_df['Interpolated'] = china_df.apply(axis=1,
-    #                                           func=lambda x: x['Quantity'] if x['Year'] not in {1959, 1960, 1961 } else nan)
-    # china_df['Interpolated'] = china_df['Interpolated'].interpolate().astype(int)
-    # LOGGER.info('excess deaths estimate: %d', china_df['Quantity'].sum() - china_df['Interpolated'].sum())
-    # china_df['Actual'] = china_df['Quantity']
-    # small_plot_df = melt(frame=china_df, id_vars=['Year'], value_vars=['Actual', 'Interpolated'], value_name='Deaths')
-    # figure_, axes_ = subplots()
-    # _ = lineplot(ax=axes_, data=small_plot_df, x='Year', y='Deaths', hue='variable')
-    # fname_ = '{}{}_lineplot.png'.format(OUTPUT_FOLDER, 'china_interpolated')
-    # tight_layout()
-    # savefig(format='png', fname=fname_, )
-    # close(fig=figure_)
-    # LOGGER.info('saved plot in %s', fname_)
-
-
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
