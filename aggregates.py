40e4b40eff00446c693478f316a380e58a98e06d
Mike DeLong <mikedelong@outlook.com>
2022-11-19 13:10:05 -0500

    feature: introduce a second group of regions; we're probably done doing ths scatterplot with regions now.

--- aggregates.py
+++ aggregates.py
@@ -23,10 +23,10 @@ AGGREGATE_COLUMNS = ['Region, subregion, country or area *', 'Crude Death Rate (
 DATA_FOLDER = './data/'
 INPUT_FILE = 'WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT_REV1.xlsx'
 OUTPUT_FOLDER = './plot/'
+OTHER_REGIONS = {'World','Development Group','Special other', 'Income Group',}
 REGION_SUBREGION = {'World', 'Region', 'Subregion', }
 RENAME_COLUMNS = {'Crude Death Rate (deaths per 1,000 population)': 'Crude Death',
                   'Region, subregion, country or area *': 'Aggregate', }
-SAVE_WORLD_DATA = False
 SEABORN_STYLE = 'darkgrid'
 
 if __name__ == '__main__':
@@ -43,33 +43,36 @@ if __name__ == '__main__':
     df = read_excel_dataframe(io=data_file, header=16, usecols=COLUMNS)
     LOGGER.info('loaded %d rows from %s', len(df), data_file)
 
-    codes_df = df[['Region, subregion, country or area *', 'Type', 'Parent code', 'Location code']].drop_duplicates()
-    codes_df = codes_df[codes_df['Type'].isin(REGION_SUBREGION)]
-    # map the regions' parent codes onto their location codes
-    codes_df['Parent code'] = codes_df.apply(axis=1,
-                                             func=lambda x: x['Location code'] if x['Type'] == 'Region' else x[
-                                                 'Parent code'])
-    # discard all the countries, areas, labels, and separators
-    aggregate_df = df[df['Type'].isin(REGION_SUBREGION)][AGGREGATE_COLUMNS].rename(columns=RENAME_COLUMNS)
-    codes_df = codes_df.rename(columns=RENAME_COLUMNS)
-
-    aggregate = ['Aggregate']
-    x_var = 'Mean Crude Death'
-    y_var = 'std dev Crude Death'
-    plot_df = aggregate_df.groupby(by=aggregate).mean().rename(columns={'Crude Death': x_var}).merge(
-        right=aggregate_df.groupby(by=aggregate).std().rename(columns={'Crude Death': y_var}),
-        on=aggregate).reset_index()
-
-    # merge/join in the parent codes so we can use them for the hues below
-    plot_df = plot_df.merge(right=codes_df, on='Aggregate')
-
     set_style(style=SEABORN_STYLE)
-    figure_scatterplot, axes_scatterplot = subplots()
-    hue = 'Parent code'
-    result_scatterplot = lmplot(data=plot_df, x=x_var, y=y_var, fit_reg=False, hue=hue, legend=False, aspect=2, )
-    label_point(x=plot_df[x_var], y=plot_df[y_var], val=plot_df['Aggregate'], ax=gca())
-    tight_layout()
-    savefig(fname=OUTPUT_FOLDER + 'aggregate_mean_stddev_scatterplot.png', format='png')
-    close(fig=figure_scatterplot)
+    for index, aggregate in enumerate([REGION_SUBREGION, OTHER_REGIONS]):
+        codes_df = df[['Region, subregion, country or area *', 'Type', 'Parent code', 'Location code']].drop_duplicates()
+        codes_df = codes_df[codes_df['Type'].isin(aggregate)]
+        # map the regions' parent codes onto their location codes
+        codes_df['Parent code'] = codes_df.apply(axis=1,
+                                                 func=lambda x: x['Location code'] if x['Type'] == 'Region' else x[
+                                                     'Parent code'])
+        # discard all the countries, areas, labels, and separators
+        aggregate_df = df[df['Type'].isin(aggregate)][AGGREGATE_COLUMNS].rename(columns=RENAME_COLUMNS)
+        codes_df = codes_df.rename(columns=RENAME_COLUMNS)
+
+        this_column = ['Aggregate']
+        x_var = 'Mean Crude Death'
+        y_var = 'std dev Crude Death'
+        plot_df = aggregate_df.groupby(by=this_column).mean().rename(columns={'Crude Death': x_var}).merge(
+            right=aggregate_df.groupby(by=this_column).std().rename(columns={'Crude Death': y_var}),
+            on=this_column).reset_index()
+
+        # merge/join in the parent codes so we can use them for the hues below
+        plot_df = plot_df.merge(right=codes_df, on=this_column)
+
+        figure_scatterplot, axes_scatterplot = subplots()
+        hue = 'Parent code'
+        result_scatterplot = lmplot(data=plot_df, x=x_var, y=y_var, fit_reg=False, hue=hue, legend=False, aspect=2, )
+        label_point(x=plot_df[x_var], y=plot_df[y_var], val=plot_df['Aggregate'], ax=gca())
+        tight_layout()
+        fname = OUTPUT_FOLDER + 'aggregate_mean_stddev_scatterplot_{}.png'.format(index+1)
+        LOGGER.info('saving plot to %s', fname)
+        savefig(fname=fname, format='png')
+        close(fig=figure_scatterplot)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
