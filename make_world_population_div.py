270ef7b449466907a2780ecd94c62a4a123f6d67
Mike DeLong <mikedelong@outlook.com>
2022-11-23 10:00:26 -0500

    refactor: use the version of read_excel_dataframe that is in common instead of using a local copy

--- make_world_population_div.py
+++ make_world_population_div.py
@@ -6,21 +6,14 @@ from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 from pathlib import Path
-from typing import Optional
-from typing import Union
 
 from arrow import now
 from pandas import DataFrame
-from pandas import read_excel
 from pandas import to_datetime
 from plotly.express import scatter
 from plotly.io import to_html
 
-
-def read_excel_dataframe(io: str, header: int, usecols: Optional[Union[list, int]]) -> DataFrame:
-    result_df = read_excel(engine='openpyxl', header=header, io=io, usecols=usecols)
-    return result_df
-
+from common import read_excel_dataframe
 
 DATA_FOLDER = './data/'
 INPUT_FILE = 'WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT_REV1.xlsx'
@@ -83,7 +76,8 @@ if __name__ == '__main__':
         }
     ).sort_values(by='date').reset_index(drop=True)
     min_date = population_df['date-as-date'].min().to_pydatetime().date()
-    population_df['serialtime'] = population_df['date-as-date'].apply(lambda x: (x.to_pydatetime().date() - min_date).days)
+    population_df['serialtime'] = population_df['date-as-date'].apply(
+        lambda x: (x.to_pydatetime().date() - min_date).days)
     # todo put dates on the x axis ticks
     scatter_plot = scatter(data_frame=population_df, x='serialtime', y='population',
                            trendline='ols',
