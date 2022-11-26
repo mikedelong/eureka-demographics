9345e0599787d7ff08d601c91ae490e83c1c69fe
Mike DeLong <mikedelong@outlook.com>
2022-11-26 14:50:02 -0500

    feature: read the CDC data in and do some initial cleanup

---
+++ read_cdc.py
@@ -0,0 +1,37 @@
+"""
+Load and parse CSV data from the CDC
+"""
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+from pathlib import Path
+
+from arrow import now
+from pandas import DataFrame
+from pandas import read_csv
+
+def read_url_csv(url: str) -> DataFrame:
+    result_df = read_csv(filepath_or_buffer=url, thousands=',', sep='\t')
+    return result_df
+
+DATA_FOLDER = './data/'
+INPUT_FILE = 'Underlying Cause of Death, 1999-2020.txt'
+
+if __name__ == '__main__':
+    TIME_START = now()
+    LOGGER = getLogger(__name__, )
+    basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
+    LOGGER.info('started')
+
+    for folder in [DATA_FOLDER, ]:
+        LOGGER.info('creating folder %s if it does not exist', folder)
+        Path(folder).mkdir(parents=True, exist_ok=True)
+
+    input_file = DATA_FOLDER + INPUT_FILE
+    df = read_url_csv(url=input_file)
+    df = df.drop(columns=['Notes', 'Year Code']).dropna()
+    unreliable_df = df[df['Crude Rate'] == 'Unreliable']
+
+    LOGGER.info(df.shape)
+
+    LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
