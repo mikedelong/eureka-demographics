73a89979b81c4a5852901f5f2834a49cfe2f0ee1
Mike DeLong <mikedelong@outlook.com>
2022-11-26 15:08:36 -0500

    feature: type conversions and calculate the crude death rate in place of the rate supplied

--- read_cdc.py
+++ read_cdc.py
@@ -10,12 +10,14 @@ from arrow import now
 from pandas import DataFrame
 from pandas import read_csv
 
+
 def read_url_csv(url: str) -> DataFrame:
     result_df = read_csv(filepath_or_buffer=url, thousands=',', sep='\t')
     return result_df
 
 DATA_FOLDER = './data/'
 INPUT_FILE = 'Underlying Cause of Death, 1999-2020.txt'
+SCALING = 1000
 
 if __name__ == '__main__':
     TIME_START = now()
@@ -29,8 +31,11 @@ if __name__ == '__main__':
 
     input_file = DATA_FOLDER + INPUT_FILE
     df = read_url_csv(url=input_file)
-    df = df.drop(columns=['Notes', 'Year Code']).dropna()
+    df = df.drop(columns=['Notes', 'Year Code', 'Crude Rate']).dropna()
+    for column in ['Year', 'Deaths', 'Population']:
+        df[column] = df[column].astype(int)
     unreliable_df = df[df['Crude Rate'] == 'Unreliable']
+    df['crude rate'] = SCALING * df['Deaths']/df['Population']
 
     LOGGER.info(df.shape)
 
