bfd9798f003df1d467006fd68b5b64a06be1fda6
Mike DeLong <mikedelong@outlook.com>
2022-11-29 12:51:47 -0500

    feature: use a rank by total deaths to split up the curves into groups

--- cdc_lineplots.py
+++ cdc_lineplots.py
@@ -26,6 +26,7 @@ COLUMNS = ['ICD-10 113 Cause List', 'ICD-10 113 Cause List Code', 'Year', 'Death
            'log10 deaths']
 DATA_FOLDER = './data/'
 INPUT_FILE = 'Wonder-cause-of-death-1999-2020.csv'
+MAKE_PLOTS = True
 OUTPUT_FOLDER = './plot_cdc/'
 
 if __name__ == '__main__':
@@ -40,23 +41,35 @@ if __name__ == '__main__':
 
     input_file = DATA_FOLDER + INPUT_FILE
 
-
-    df = read_url_csv(url=input_file, usecols=[COLUMNS[0],
-                                               COLUMNS[1], 'Year', 'Deaths', ]).rename(
-        columns={'ICD-10 113 Cause List Code' : 'Code'}
-    )
+    df = read_url_csv(url=input_file, usecols=COLUMNS[:4]).rename(columns={COLUMNS[1]: 'Code'})
     major_df = df[~df[COLUMNS[0]].str.contains('#')]
     minor_df = df[df[COLUMNS[0]].str.contains('#')]
     codes = df['Code'].unique()
-    # df = df[df['Code'].isin(codes[:10])]
-    plot_df = melt(frame=minor_df.drop(columns=[COLUMNS[0]]), id_vars=['Year', 'Code'], value_name='Deaths', ).drop(columns=['variable'])
+    # rank the codes by total count
+    total = 'total Deaths'
+    total_df = df[[COLUMNS[0], 'Code', 'Deaths']].groupby(by=[COLUMNS[0], 'Code']).sum().reset_index().rename(
+        columns={'Deaths': total})
+    total_df['rank'] = total_df[total].rank()
+    causes_ranked = DataFrame(total_df).sort_values(by=['rank'])['Code'].tolist()
 
-    figure_, axes_ = subplots()
-    _ = lineplot(ax=axes_, data=plot_df, x='Year', y='Deaths', hue='Code')
-    fname = '{}{}_lineplot.png'.format(OUTPUT_FOLDER, 'cdc_113')
-    tight_layout()
-    savefig(format='png', fname=fname, )
-    close(fig=figure_)
-    LOGGER.info('saved plot in %s', fname)
+    if MAKE_PLOTS:
+        size = 8
+        for start in range(0, len(causes_ranked), size):
+            # swap l_var and r_var to get curves labeled by their code
+            l_var, r_var = 'Code', COLUMNS[0]
+            # todo why is this producing a FutureWarning?
+            plot_df = melt(frame=df[df['Code'].isin(causes_ranked[start:start + size])].drop(columns=[l_var]),
+                           id_vars=['Year', r_var], value_name='Deaths', ).drop(columns=['variable'])
+            if r_var == COLUMNS[0]:
+                plot_df['Cause'] = plot_df[COLUMNS[0]].apply(lambda x: x[:30])
+
+            figure_, axes_ = subplots()
+            hue = [COLUMNS[0], 'Cause'][1]
+            _ = lineplot(ax=axes_, data=plot_df, x='Year', y='Deaths', hue=hue)
+            fname = '{}{}_{}_lineplot.png'.format(OUTPUT_FOLDER, 'cdc_113', start)
+            tight_layout()
+            savefig(format='png', fname=fname, )
+            close(fig=figure_)
+            LOGGER.info('saved plot in %s', fname)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
