25de573a89b376de17e841f3d49f75906e533d71
Mike DeLong <mikedelong@outlook.com>
2022-12-01 11:25:51 -0500

    feature: use Seaborn dark style; also, move the legend outside the plot

--- cdc_lineplots.py
+++ cdc_lineplots.py
@@ -16,6 +16,7 @@ from pandas import DataFrame
 from pandas import melt
 from pandas import read_csv
 from seaborn import lineplot
+from seaborn import set_style
 
 
 def read_url_csv(url: str, usecols: list) -> DataFrame:
@@ -27,11 +28,14 @@ CAUSE_MAP_FILE = 'causes_map.json'
 COLUMNS = ['ICD-10 113 Cause List', 'ICD-10 113 Cause List Code', 'Year', 'Deaths', 'Population', 'crude rate',
            'log10 deaths']
 DATA_FOLDER = './data/'
+FIGSIZE = (12, 9)
 INPUT_FILE = 'Wonder-cause-of-death-1999-2020.csv'
 MAKE_PLOTS = True
 OTHER_DATA_FOLDER = './data_cdc/'
 OUTPUT_FOLDER = './plot_cdc/'
 PLOT_SIZE = 7
+SEABORN_STYLE = 'darkgrid'
+
 
 if __name__ == '__main__':
     TIME_START = now()
@@ -64,6 +68,7 @@ if __name__ == '__main__':
     major_causes_ranked = major_causes_df['Code'].values
 
     if MAKE_PLOTS:
+        set_style(style=SEABORN_STYLE)
         # let's do the major causes together
         for case, start in enumerate(range(0, len(major_causes_ranked), PLOT_SIZE)):
             # swap l_var and r_var to get curves labeled by their code
@@ -74,16 +79,16 @@ if __name__ == '__main__':
             if r_var == COLUMNS[0]:
                 plot_df['Cause'] = plot_df[COLUMNS[0]].apply(lambda x: CAUSES_MAP[x][:30])
 
-            figure_, axes_ = subplots()
+            figure, axes = subplots(figsize=FIGSIZE)
             hue = [COLUMNS[0], 'Cause'][1]
-            _ = lineplot(ax=axes_, data=plot_df, x='Year', y='Deaths', hue=hue)
+            plot_result = lineplot(ax=axes, data=plot_df, x='Year', y='Deaths', hue=hue)
             if case == 2:
-                axes_.set(ylim=(0, 30000))
-                pass # fix y max here
+                axes.set(ylim=(0, 25000))
             fname = '{}{}_{}_lineplot.png'.format(OUTPUT_FOLDER, 'cdc_113', start)
+            plot_result.get_legend().set_bbox_to_anchor((1, 1))
             tight_layout()
             savefig(format='png', fname=fname, )
-            close(fig=figure_)
+            close(fig=figure)
             LOGGER.info('saved plot in %s', fname)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
