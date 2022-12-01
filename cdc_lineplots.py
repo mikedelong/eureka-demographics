c4d5d1c2b4d9138d45573251594df96ad916f476
Mike DeLong <mikedelong@outlook.com>
2022-12-01 14:10:28 -0500

    feature: first working version of the major causes graphs that look at all sensible

--- cdc_lineplots.py
+++ cdc_lineplots.py
@@ -13,6 +13,7 @@ from matplotlib.pyplot import savefig
 from matplotlib.pyplot import subplots
 from matplotlib.pyplot import tight_layout
 from pandas import DataFrame
+from pandas import concat
 from pandas import melt
 from pandas import read_csv
 from seaborn import lineplot
@@ -33,7 +34,7 @@ INPUT_FILE = 'Wonder-cause-of-death-1999-2020.csv'
 MAKE_PLOTS = True
 OTHER_DATA_FOLDER = './data_cdc/'
 OUTPUT_FOLDER = './plot_cdc/'
-PLOT_SIZE = 7
+PLOT_SIZE = 9
 SEABORN_STYLE = 'darkgrid'
 
 if __name__ == '__main__':
@@ -52,6 +53,12 @@ if __name__ == '__main__':
     input_file = DATA_FOLDER + INPUT_FILE
 
     df = read_url_csv(url=input_file, usecols=COLUMNS[:4]).rename(columns={COLUMNS[1]: 'Code'})
+    # we need to touch up the original DataFrame by adding a zero value for COVID for 2019
+    # otherwise our line plots don't capture COVID
+    covid_df = df[df['Code'] == 'GR113-137'].copy(deep=True)
+    covid_df['Year'] = 2019
+    covid_df['Deaths'] = 0
+    df = concat([df, covid_df])
 
     # we can rank by the total deaths (by summing each cause of death along the time series)
     # or we can rank by the most recent year
@@ -65,17 +72,18 @@ if __name__ == '__main__':
     # build the max year ranks if we want to rank by the most recent year
     max_year_deaths = '{} Deaths'.format(max_year)
     max_year_df = df[df['Year'] == max_year][[COLUMNS[0], 'Code', 'Deaths']].reset_index().rename(
-        columns={'Deaths': max_year_deaths})
+        columns={'Deaths': max_year_deaths}).drop(columns=['index'])
     max_year_df['rank'] = max_year_df[max_year_deaths].rank(ascending=False)
 
+    # choose which DataFrame to use for the ranking
     major_causes = [item for item in df[COLUMNS[0]].unique().tolist() if item.startswith('#')]
-    major_causes_df = total_df[total_df[COLUMNS[0]].isin(major_causes)].sort_values(by=['rank'], ascending=True)
+    major_causes_df = max_year_df[max_year_df[COLUMNS[0]].isin(major_causes)].sort_values(by=['rank'], ascending=True)
     major_causes_ranked = major_causes_df['Code'].values
 
     if MAKE_PLOTS:
         set_style(style=SEABORN_STYLE)
         # let's do the major causes together
-        for case, start in enumerate(range(0, len(major_causes_ranked), PLOT_SIZE)):
+        for start in range(0, len(major_causes_ranked), PLOT_SIZE):
             # swap l_var and r_var to get curves labeled by their code
             l_var, r_var = 'Code', COLUMNS[0]
             plot_df = melt(
@@ -86,9 +94,8 @@ if __name__ == '__main__':
                 plot_df['Cause'] = plot_df[COLUMNS[0]].apply(lambda x: CAUSES_MAP[x][:30])
 
             figure, axes = subplots(figsize=FIGSIZE)
-            plot_result = lineplot(ax=axes, data=plot_df, x='Year', y='Deaths', hue=[COLUMNS[0], 'Cause'][1])
-            if case == 2:
-                axes.set(ylim=(0, 25000))
+            plot_result = lineplot(ax=axes, data=plot_df, estimator=None,
+                                   x='Year', y='Deaths', hue=[COLUMNS[0], 'Cause'][1])
             fname = '{}{}_{}_lineplot.png'.format(OUTPUT_FOLDER, 'cdc_113', start)
             plot_result.get_legend().set_bbox_to_anchor((1, 1))
             tight_layout()
